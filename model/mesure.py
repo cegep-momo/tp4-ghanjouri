debdd624fc417502c1b1849f21883b4c868d2184
Nermine <nerminemahdi@gmail.com>
2025-04-14 12:19:55 -0400

    Creation de la class mesure

---
+++ model/mesure.py
@@ -0,0 +1,6 @@
+from datetime import datetime
+
+class Mesure:
+    def __init__(self, dateHeureMesure, dataMesure):
+        self.dateHeureMesure = dateHeureMesure
+        self.dataMesure = dataMesure            
