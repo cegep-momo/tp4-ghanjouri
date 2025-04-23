95131df3f6eefef1e4de101ad0f9695bc16f4f0e
Nermine ghanjouri <nerminemahdi@gmail.com>
2025-04-22 22:41:13 -0400

    Fonctionnalité : programmer la class Conroler.py

---
+++ controler/controler.py
@@ -0,0 +1,77 @@
+from model.platine import Platine
+from model.mesure import Mesure
+from view.view import Vue
+import time
+
+class Controleur:
+    def __init__(self):
+
+        self.platine = Platine()
+        self.vue = Vue()
+        self.systeme_en_marche = False
+
+    def demarrer_systeme(self):
+        
+        # on met le système en marche et on affiche un message sur l'écran
+       
+        self.systeme_en_marche = True
+        print("Système démarré")
+        self.vue.afficher_message("Systeme", "Demarer")
+
+    def arreter_systeme(self):
+        
+        # on arrête le système et affiche un message sur l'écran
+        
+        self.systeme_en_marche = False
+        print("Système arrêté")
+        self.vue.afficher_message("Systeme", "Arreter")
+
+    def prendre_mesure(self):
+        
+        # on prend une mesure avec le capteur et affiche la distance sur l'écran et sauvegarde dans JSON
+        
+        print("Prise de mesure...")
+        self.vue.afficher_message("Prise", "Mesure...")
+        
+        mesure_data = self.platine.lire_mesure()
+        if mesure_data:
+            mesure = Mesure(
+                dateHeureMesure = mesure_data["date"].strftime("%Y-%m-%d %H:%M:%S"),
+                dataMesure = {
+                    "Distance (cm)": mesure_data["distance"]
+                }
+            )
+            print(mesure.afficherMesure())
+            self.vue.afficher_distance(mesure_data["distance"])
+            mesure.sauvegarderJson()
+
+    def programme(self):
+        
+        # Boucle pour gérer l'appui sur les boutons et l'état du système.
+        
+        print("Système prêt. En attente de bouton...")
+
+        try:
+            while True:
+                bouton = self.platine.bouton_appuye()
+
+                if bouton == "demarrer":
+                    # Changer l'état du système
+                    if self.systeme_en_marche:
+                        self.arreter_systeme()
+                    else:
+                        self.demarrer_systeme()
+                    time.sleep(0.5)
+
+                elif bouton == "mesure" and self.systeme_en_marche:
+                    # Si système en marche, prendre une mesure
+                    self.prendre_mesure()
+                    time.sleep(0.5)
+
+                else:
+                    time.sleep(0.1)
+
+        except KeyboardInterrupt:
+            print("\nArrêt du programme par l'utilisateur.")
+            self.vue.effacer()
+            time.sleep(1)
