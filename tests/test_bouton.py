4e5782ff3c3d4fe15fa32e4e3d5d89b7aa203031
Nermine ghanjouri <nerminemahdi@gmail.com>
2025-04-24 21:32:01 -0400

    Commit obligatoire test unitaire

--- tests/test_bouton.py
+++ tests/test_bouton.py
@@ -8,23 +8,24 @@ Device.pin_factory = MockFactory()
 
 class TestPlatine(unittest.TestCase):
     def setUp(self):
-        # Créer une platine simulée
+       
         self.platine = Platine()
 
-    def test_bouton_demarrer_presse(self):
+    def test_bouton_demarrer_appuier(self):
         # Test lors d'un appui sur le bouton demarrer
         self.platine.boutons["demarrer"].pin.drive_low()
         bouton_presse = self.platine.bouton_appuye()
         self.assertEqual(bouton_presse, "demarrer")
 
         # Test lors d'un appui sur le bouton mesurer
-    def test_bouton_mesure_presse(self):
+    def test_bouton_mesure_appuier(self):
         self.platine.boutons["mesure"].pin.drive_low()
         bouton_presse = self.platine.bouton_appuye()
         self.assertEqual(bouton_presse, "mesure")
+        
 
     # Test pour verifier que aucun bouton appuyé
-    def test_bouton_non_presse(self):
+    def test_bouton_non_appuier(self):
        
         bouton_presse = self.platine.bouton_appuye()
         self.assertIsNone(bouton_presse)
